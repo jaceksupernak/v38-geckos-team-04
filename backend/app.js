c3ca738ecdba2c7a14734764c4d4d647998eeb3e
Jacek Supernak <jj.supernak@gmail.com>
2024-01-15 02:52:15 +0000

    install cors

--- backend/app.js
+++ backend/app.js
@@ -1,40 +1,51 @@
 // import dotenv, express, serverApiVerson and mongoose modules
-require('dotenv').config();
-const express = require('express');
-const { ServerApiVersion } = require('mongodb');
-const mongoose = require('mongoose');
+require('dotenv').config()
+const express = require('express')
+const { ServerApiVersion } = require('mongodb')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const corsOptions = {
+  origin: 'https://develappchingu.netlify.app',
+}
 
 // import main route
-const API = require('./routes/api');
+const API = require('./routes/api')
 
 // expose the express api into the app const
-const app = express();
+const app = express()
 
 // read the PORT environment variable into port
-const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.DB_URI;
+const PORT = process.env.PORT || 5000
+const MONGODB_URI = process.env.DB_URI
+
+app.use(cors(corsOptions))
 
 // parse application/json content-type
-app.use(express.json());
+app.use(express.json())
 
 // use the mainRoute
-app.use("/api", API);
+app.use('/api', API)
 
 // mongoose connection options
 const options = {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-    serverApi: ServerApiVersion.v1
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 }
 
 // create database connection
-mongoose.connect(MONGODB_URI, options).then(result => {
-    console.log('Connected successfully to the Database');
-}).catch(error => {
-    console.log('Connection failed! Please ensure you have a network connection.');
-})
+mongoose
+  .connect(MONGODB_URI, options)
+  .then((result) => {
+    console.log('Connected successfully to the Database')
+  })
+  .catch((error) => {
+    console.log(
+      'Connection failed! Please ensure you have a network connection.'
+    )
+  })
 
 // create server
 app.listen(PORT, () => {
-    console.log(`Visit http://localhost:${PORT}/api on a browser`);
-});
+  console.log(`Visit http://localhost:${PORT}/api on a browser`)
+})
